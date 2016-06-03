9958099906e82aa722c997b6592298a943f5a7bf
Tim Ververs <TimVervers@users.noreply.github.com>
2016-06-03 15:15:34 +0200

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,19 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+
+namespace aspnetcoreapp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .UseUrls("http://192.168.249.11:80/")
+                .Build();
+
+            host.Run();
+        }
+    }
+}
