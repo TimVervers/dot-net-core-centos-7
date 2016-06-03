36cd2da073377328451e7e6d500ef2b214687310
Tim Ververs <t.ververs@coolblue.nl>
2016-06-03 15:33:44 +0200

    .

--- Program.cs
+++ Program.cs
@@ -6,15 +6,9 @@ namespace aspnetcoreapp
     public class Program
     {
         public static void Main(string[] args)
-        {
-            var configuration = new ConfigurationBuilder()
-                .AddCommandLine(args)
-                .Build();
-                
+        {                
             var host = new WebHostBuilder()
                 .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseConfiguration(configuration)
                 .UseStartup<Startup>()
                 .Build();
 
