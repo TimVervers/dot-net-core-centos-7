40814f07b2a79b41f0de415e9ca6fb2121a3cf84
Tim Ververs <TimVervers@users.noreply.github.com>
2016-06-03 15:36:33 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace aspnetcoreapp
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseStartup<Startup>()
+                .UseUrls("http://*:80/")
                 .Build();
 
             host.Run();
